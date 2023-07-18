1391c93f30d9c46e29758c033af7d070abbfdee9
Zarman-oss <zarmanimran@gmail.com>
2023-07-18 10:49:12 +0500

    search bar commit 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import SearchBar from './Components/SearchBar';
+import searchImages from './api';
 
 function App() {
+  const handleSubmit = (term) => {
+    console.log('Do a serach with', term);
+    searchImages(term);
+  };
   return (
-    <div className="div">
-      <SearchBar />
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
     </div>
   );
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,4 +1,22 @@
-function SearchBar() {
-  return <div>App</div>;
+import { useState } from 'react';
+
+function SearchBar({ onSubmit }) {
+  const [item, setItem] = useState('');
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit('grg');
+  };
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input value={item} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 export default SearchBar;
