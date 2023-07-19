efc8c10cd7f36f170a402d67349b4d8bce6c5278
Zarman-oss <zarmanimran@gmail.com>
2023-07-20 04:36:40 +0500

    image-list initail commit

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,4 +1,10 @@
-function ImageList() {
-  return <div>App</div>;
+import ImageShow from './ImageShow';
+
+function ImageList({ images }) {
+  const renderedImages = images.map((images) => {
+    return <ImageShow image={images} />;
+  });
+
+  return <div>{renderedImages}</div>;
 }
 export default ImageList;
